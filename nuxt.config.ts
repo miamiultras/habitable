8f2458e7378ffb6232cd28731e5bb794f8253a81
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-07-19 20:31:30 +0200

    setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
-  css: ['@/assets/main.css'],
-})
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
+  css: ["@/assets/main.css"],
+});
