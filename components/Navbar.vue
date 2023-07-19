78a5643f15524208a09ebc5458754ab0e0958c43
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-07-19 20:32:21 +0200

    create basic navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="z-10 w-full p-4 px-8 flex items-center justify-between text-white">
+    <NuxtLink href="/" class="text-2xl font-semibold hover:underline">
+      Habitable
+    </NuxtLink>
+    <ul class="flex">
+      <li class="ml-6">
+        <NuxtLink href="/about" class="text-2xl font-semibold hover:underline">
+          About
+        </NuxtLink>
+      </li>
+      <li class="ml-6">
+        <NuxtLink href="/account" class="text-2xl font-semibold hover:underline">
+          Account
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
