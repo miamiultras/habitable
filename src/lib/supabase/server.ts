849f381a0eb9cef69c010f9ba2e9b4d596b6beaf
eeeeeyyyphophopho <26793642+miamiultras@users.noreply.github.com>
2026-02-26 19:35:41 +0100

    fix type errors

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -9,19 +9,17 @@ export async function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
+        get(name: string) {
+          return cookieStore.get(name)
+        },
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+        set(name: string, value: string) {
+          cookieStore.set(name, value)
+        },
+        delete(name: string) {
+          cookieStore.delete(name)
         },
       },
     }
